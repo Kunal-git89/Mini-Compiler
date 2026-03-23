193bba099429e515f6d14cc9d2ce0a6dc89f453b
Kunal-git89 <kunalkumargit2125@gmail.com>
2026-03-23 12:11:15 +0530

    Semantic 2
    bug

--- src/Transpiler/Semantic/Semantic_Analyzer.java
+++ src/Transpiler/Semantic/Semantic_Analyzer.java
@@ -1,6 +1,31 @@
 package Transpiler.Semantic;
 
+import Transpiler.AST.*;
+
+import java.util.*;
+
 public class Semantic_Analyzer
 {
-    
+    private SymbolTable_Manager symboltable = new SymbolTable_Manager();
+    private List<ASTNode> AST = new ArrayList<>();
+
+    public Semantic_Analyzer(List<ASTNode> l)
+    {
+        AST = l;
+    }
+
+    public void start()
+    {
+        symboltable.addScope(); //Global scope start
+        for(ASTNode node : AST)
+        {
+
+        }
+        symboltable.existScope(); //Global scope end
+    }
+
+    private void analyze() // Actual analysis
+    {
+
+    }
 }
