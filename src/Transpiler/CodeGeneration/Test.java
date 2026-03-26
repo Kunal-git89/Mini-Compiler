043a4d4d4243c276828c06d29537d0286bfc08fa
Kunal-git89 <kunalkumargit2125@gmail.com>
2026-03-26 20:22:40 +0530

    Working

--- src/Transpiler/CodeGeneration/Test.java
+++ src/Transpiler/CodeGeneration/Test.java
@@ -1,77 +1,51 @@
 package Transpiler.CodeGeneration;
 
-import java.nio.file.*;
-import java.sql.SQLOutput;
 import java.util.*;
 import Transpiler.AST.*;
 import java.io.*;
 
-public class CodeGenerator
+public class Test
 {
     private List<ASTNode> program ;
     private String filename;
     private List<String> code = new ArrayList<>();
-    private FileWriter writer;
-    private String[] classes;
-    public File file;
     public int indent = 0;
-
-    public CodeGenerator(List<ASTNode> l , String s , File f , String[] c) throws IOException
+    public Test(List<ASTNode> l , String s)
     {
         program = l;
         filename = s;
-        file = f;
-        classes = c;
     }
 
-    public void start() throws IOException
+    public void start()
     {
-        writer = new FileWriter(file);
         emit("import java.util.*;");
         emit("");
-        writeclasses();
         emit("public class " + filename + " {");
         indent++;
         emit("public static void main (String[] args) { ");
-        indent++;
 
-
-        //My code starts here
-        emit("Range r = new Range(1 , 10);");
+        // my code
         emit("System.out.println(\"Hello World\");");
-        //My code ends here
 
-
-        indent--;
         emit("}");
         indent--;
         emit("}");
+
         outputCode();
-        writer.close();
         compileAndRun();
     }
-
-    private void writeclasses() throws IOException
-    {
-        for(String str : classes)
-        {
-            String s = Files.readString(Path.of(str));
-            s = s.replace("package Transpiler.CodeGeneration;" , "");
-            emit(s);
-            emit("\n");
-        }
-    }
-
 
     private void outputCode()
     {
         try
         {
+            FileWriter writer = new FileWriter(filename + ".java");
             for(String line : code)
             {
                 writer.write(line);
                 writer.write("\n");
             }
+            writer.close();
         }
         catch(IOException e)
         {
